4b4967784d9e23c3321d484458196bb93cb11955
Akanimoh12 <akanimohjohnson2003@gmail.com>
2025-04-14 00:02:03 +0100

    array memory

--- contracts/DataTypes.sol
+++ contracts/DataTypes.sol
@@ -2,5 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract DataTypes {
-    
+    uint[] numbers = [1,2,3,4,5];
+
+    function getNumbers() public view returns (uint[] memory) {
+        return numbers;
+    }
 }
